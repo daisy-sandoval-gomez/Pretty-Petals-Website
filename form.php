976da3a7fb9f04114308f2690184fb626174f8c5
Daisy Sandoval <sandoval95d@gmail.com>
2022-05-16 19:03:31 -0700

    Pretty Petals Commit

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Pretty Petals</title>
+</head>
+<body>
+<?php 
+
+
+//variables:
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$comments = $_POST['comments'];
+
+//Print the received data: 
+print "<h1>Thank You for Signing Up!</h1>";
+
+?>
+</body>
+</html>
